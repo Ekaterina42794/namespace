a1238b41ade84412ffe8aaf20cc022d86186361a
Катя <katja48tab42794@gmail.com>
2024-12-23 23:06:30 +0300

    23.12

---
+++ ins.py
@@ -0,0 +1,10 @@
+from module_4_.alg import *
+data_1=list(map(int,input('Введите числа через пробел: ').split())) # 1 2 3 4
+data_2=list(map(int,input('Введите числа через пробел: ').split()))
+data_3=list(map(int,input('Введите числа через пробел: ').split()))
+bubble_sort(data_1)
+print(data_1)
+
+selection_sort(data_2)
+print(data_2)
+
