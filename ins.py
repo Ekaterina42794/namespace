6a195954db728193e36a8e2487e58657701d26ad
Катя <katja48tab42794@gmail.com>
2024-12-23 23:14:56 +0300

    23.12

--- ins.py
+++ ins.py
@@ -1,10 +1,15 @@
-from module_4_.alg import *
+from encodings.punycode import insertion_sort
+
+from module_42.alg import *
 data_1=list(map(int,input('Введите числа через пробел: ').split())) # 1 2 3 4
 data_2=list(map(int,input('Введите числа через пробел: ').split()))
 data_3=list(map(int,input('Введите числа через пробел: ').split()))
 bubble_sort(data_1)
-print(data_1)
+print('пузырьковая сортровка:_ ',data_1)
 
 selection_sort(data_2)
-print(data_2)
+print('сортировка выборок:_ ',data_2)
+
+#insertion_sort(data_3)
+#print('сортровка вставкой:_ ',data_3)
 
