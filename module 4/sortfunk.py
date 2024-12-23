aed070873e19aa4010848ea25032bff16ef9722c
Катя <katja48tab42794@gmail.com>
2024-12-23 19:34:39 +0300

    23.12 init

--- module 4/sortfunk.py
+++ module 4/sortfunk.py
@@ -2,9 +2,9 @@ print("Пузырьковая сортировка")
 nums = [5, 7, 9, 2, 1, 4, 3, 6, 8]
 
 
-def bubble_sort(ls):  # пузырьковая сортировка o(n2)
-    swapped = True  # переменная для сравнения
-    while swapped:
+def bubble_sort(ls):    # пузырьковая сортировка o(n2)
+    swapped = True      # поменялись местами = True
+    while swapped:      # пока поменялись местами
         swapped = False
         for i in range(len(ls) - 1):
             if ls[i] > ls[i + 1]:
@@ -19,7 +19,7 @@ print(" сортировка Выборка")
 nums_1 = [4, 3,5, 7, 9, 1,2,  6, 8]
 def selection_sort(ls):
     for i in range(len(ls)):
-        lowest = i
+        lowest = i          # самый низкий=i
         for j in range(i+1, len(ls)):
             if ls[j]<ls[lowest]:
                 lowest=j
